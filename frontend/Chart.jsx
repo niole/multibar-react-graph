dd3e6edc1c216ac88ddc27362c07230e161cb5c7
Niole Nelson <niolenelson@gmail.com>
2015-07-10 23:57:55 -0400

    can display series of data, have axes labels

--- frontend/Chart.jsx
+++ frontend/Chart.jsx
@@ -1,16 +1,27 @@
 "use strict";
 
-var React = require('react');
+var React = require('react/addons');
+//var Legend = require('./Legend.jsx');
 
 var Chart = React.createClass({
+  getDefaultProps: function() {
+    return {
+      numberMap: {0:"zero",1:"one",2:"two",
+                  3:"three",4:"four",5:"five",
+                  6:"six",7:"seven",8:"eight",
+                  9:"nine",10:"ten"}
+    };
+  },
+
   propTypes: {
-    data: React.PropTypes.array,
+    color: React.PropTypes.array,
     title: React.PropTypes.array,
     xHeader: React.PropTypes.string,
     yHeader: React.PropTypes.string,
     width: React.PropTypes.number,
     height: React.PropTypes.number
   },
+
   render: function(){
     var bars = [];
     //TODO add width between bars option
@@ -25,8 +36,9 @@ var Chart = React.createClass({
     });
 
     this.props.data.forEach( function(entity,i){
+      var height = '';
       for (var k in entity){
-        var height = ((this.props.height*entity[k])/tallest).toString();
+        height = ((this.props.height*entity[k])/tallest).toString();
         bars.push( React.createElement('div',{
                                               style: {
                                                       width: barW,
@@ -34,13 +46,13 @@ var Chart = React.createClass({
                                                       backgroundColor: "white",
                                                       float: "right",
                                                       position: "relative"
-                                                      }
+                                                      },
+                                              className: "negative-space"
                                               },
           React.createElement('div',{
                                     style: {
                                       height: height,
                                       width: barW,
-                                      backgroundColor: "blue",
                                       borderStyle: "solid",
                                       borderWidth: "1",
                                       borderColor: "black",
@@ -48,31 +60,56 @@ var Chart = React.createClass({
                                       position: "absolute",
                                       bottom: 0
                                     },
-                                    className: this.props.titles[i]
-                                    })
+                                    className: this.props.numberMap[i]
+                                      })
                     )
                  );
       }
-    }.bind(this));
+     }.bind(this));
 
     return (
       React.createElement('div',{
                                 style:{
                                   borderColor: "black",
                                   borderStyle: "solid",
-                                  borderWidth: "1px"
-                                      }
+                                  borderWidth: "1px",
+                                  textAlign: "center",
+                                  position: "relative",
+                                  display: "inline-block",
+                                  width: parseInt(this.props.width)+parseInt(this.props.width/7),
+                                  height: parseInt(this.props.height)+parseInt(this.props.height/7)
+                                      },
                                 },
         React.createElement('div',{
               className: "chart-container",
               style: {
+                marginLeft: "70px",
                 width: this.props.width+"px",
-                height: this.props.height+"px",
-                padding: "50px"
+                height: this.props.height+"px"
               }
               },
               bars
-        )
+        ),
+
+        React.createElement('div',{
+                                style: {
+                                  position: "absolute",
+                                  bottom: "7%",
+                                  right: "60%"
+                                }
+                                }, this.props.xHeader),
+        React.createElement('div',{
+                                  style: {
+                                    left: 0,
+                                    bottom: "50%",
+                                    position: "absolute",
+                                    webkitTransform: 'rotate(90deg)',
+                                    mozTransform: 'rotate(90deg)',
+                                    msTransform: 'rotate(90deg)',
+                                    oTransform: 'rotate(90deg)',
+                                    transform: 'rotate(90deg)'
+                                  }
+                                  }, this.props.yHeader)
       )
     );
   }
--- frontend/Data.js
+++ frontend/Data.js
@@ -1,4 +1,5 @@
 module.exports = {
-  data: [{date1:5, date2:4, date3:9, date4:9, date5:1,date6:3}],
-  titles: ["cats"]
+  data: [{date1:5, date2:4, date3:9, date4:9, date5:1,date6:3},
+        {date1:1, date2:7, date3:10, date4:3, date5:1,date6:9}],
+  titles: ["cats","dogs"]
 };
